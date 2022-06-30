8ad86d063e8cbf031aeb3bcb80ca30f709a02f6a
Jose Lemuz <lemuz.jose@gmail.com>
2022-06-29 20:06:50 -0700

    Initial project with Navbar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,20 @@
+export default function Nav(){
+    return <nav className="nav">
+        <a href="/" className="site-title"> Jose Lemuz</a>
+        <ul>
+            <li>
+                <a href="about">About Me</a>
+            </li>
+            <li>
+                <a href="portfolio">Portfolio</a>
+            </li>
+            <li>
+                <a href="contact">Contact</a>
+            </li>
+            <li>
+                <a href="resume">Resume</a>
+            </li>
+        </ul>
+
+    </nav>
+}
