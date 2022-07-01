24e1fade5463a1a4ad118d316c859ddccee954e2
Jose Lemuz <lemuz.jose@gmail.com>
2022-06-30 22:42:07 -0700

    updated README

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 export default function Footer(){
     return <footer>
       <h1>
-        <a href="https://jlemuz.github.io"><i className="bi bi-github"></i></a>
+        <a href="https://github.com/jlemuz"><i className="bi bi-github"></i></a>
       </h1>
     </footer>
 }
